9e5e95fd03b2ec81d393c0902a1a71630952ad0a
gitmightbeshit <dataisservixe@gmail.com>
2025-02-13 14:28:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const click=document.getElementById("yes");
+const div=document.getElementById("nice");
+const div1=document.getElementById("thank");
+
+click.addEventListener("click",event =>{
+    div.style.display="block";
+    div1.style.display="block";
+    
+});
+
